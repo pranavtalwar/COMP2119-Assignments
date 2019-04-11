b62da9cc807edfa5c407e7168e6839907a64dd91
Pranav Talwar <talwar.pranav123@gmail.com>
2019-04-11 23:51:35 +0800

    add map

--- Programming Assignment 2/hmencoder.cpp
+++ Programming Assignment 2/hmencoder.cpp
@@ -5,12 +5,36 @@
 #include<fstream>
 #include<cstdlib>
 #include<map>
+#include<iterator>
 using namespace std;
 class node{
 private:
   string code;
-  
-}
+  int frequency;
+public:
+  node(){
+    code = "";
+    frequency = 1;
+  }
+  void setCode(string code){
+    this->code = code;
+  }
+  void setFrequency(int frequency){
+    this->frequency = frequency;
+  }
+  string getCode(){
+    return this->code;
+  }
+  int getFrequency(){
+    return this->frequency;
+  }
+  void increaseFrequency(){
+    this->frequency++;
+  }
+  void addCode(string addString){
+    this->code = addString + this->code;
+  }
+};
 int main(int argc, char** argv) {
   string filecontent;
   string sentence;
@@ -23,6 +47,20 @@ int main(int argc, char** argv) {
   while(getline(ifile,sentence)){
     filecontent+=sentence;
   }
-
+  map<char, node> Dictionary;
+  for(int i=0;i<filecontent.length();i++){
+    if(Dictionary.find(filecontent[i])==Dictionary.end()){
+      node newnode;
+      Dictionary[filecontent[i]] = newnode;
+    }
+    else{
+      Dictionary[filecontent[i]].increaseFrequency();
+    }
+  }
+  map<char, node>::iterator itr;
+  for(itr=Dictionary.begin();itr!=Dictionary.end();itr++)
+  {
+    cout<<(*itr).first<<" : "<<(*itr).second.getFrequency()<<endl;
+  }
   return 0;
 }
